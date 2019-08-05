668f64ae4c6c3d7170fb4ec60f4f9d076e34487d
“Paul <prowori2@gmail.com>
2019-08-05 20:55:43 +0300

    Add Todos component, set up store

---
+++ src/store/modules/todos.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line
+import axios from "axios";
+
+const state = {
+  todos: [{ id: 1, title: "Todo One" }, { id: 2, title: "Todo Two" }]
+};
+
+const getters = {
+  allTodos: state => {
+    return state.todos;
+  }
+};
+
+const actions = {};
+
+const mutations = {};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+};
